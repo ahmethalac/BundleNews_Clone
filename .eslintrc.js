8e6459f19e1ae8c73d6c9444d41a1786603e6e44
ahmethalac <ahmetfeyzihalac@gmail.com>
2021-08-08 02:11:08 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    extends: 'airbnb',
+    plugins: [
+        'react',
+        'react-native',
+        'react-hooks',
+        'react-redux',
+        'redux-saga'
+    ],
+    parser: 'babel-eslint',
+    env: {
+        jest: true,
+        'react-native/react-native': true,
+    },
+    rules: {
+        'no-use-before-define': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/prop-types': 'off',
+        'comma-dangle': 'off',
+        'padded-blocks': 'off',
+        'arrow-body-style': 'off',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-native/no-unused-styles': 2,
+        'react-native/split-platform-components': 2,
+        'react-native/no-inline-styles': 2,
+        'react-native/no-raw-text': 2,
+        'react-native/no-single-element-style-arrays': 2,
+        'no-multiple-empty-lines': [2, {
+            max: 1,
+            maxEOF: 0,
+            maxBOF: 0
+        }],
+        indent: ['error', 4, {
+            ignoredNodes: ['JSXElement *'],
+            SwitchCase: 1
+        }],
+        'react/jsx-indent': ['error', 4],
+        'import/prefer-default-export': 0,
+        'import/no-extraneous-dependencies': 0,
+        'no-case-declarations': 0,
+        'react/jsx-props-no-spreading': 0,
+        'react/jsx-indent-props': [2, 4],
+        'global-require': 0
+    },
+    globals: {
+        fetch: false
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
+};
