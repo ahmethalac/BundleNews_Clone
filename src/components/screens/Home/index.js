28a83fdc9b29dada36a88aae3c7c44a1f931ef1b
ahmethalac <ahmetfeyzihalac@gmail.com>
2021-08-10 01:41:37 +0300

    use package for masonry layout

--- src/components/screens/Home/index.js
+++ src/components/screens/Home/index.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
-    View, StatusBar, ScrollView, RefreshControl
+    View, StatusBar
 } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import MasonryList from '@react-native-seoul/masonry-list';
 import { fetchAllNews } from '../../../actions/newsActions';
 import { useCustomColorScheme } from '../../../helpers/hooks';
 import { languageSelector, layoutSelector } from '../../../selectors';
 import { newsSelector } from '../../../selectors/newsSelectors';
 import NewCard from './NewCard';
-import styles from './styles';
 
 export default function Home() {
     const colorScheme = useCustomColorScheme();
@@ -17,10 +17,6 @@ export default function Home() {
     const layout = useSelector(layoutSelector);
     const getNews = useSelector(newsSelector);
     const [refreshing, setRefreshing] = useState(false);
-
-    useEffect(() => {
-        dispatch(fetchAllNews({ language }));
-    }, [dispatch, language]);
 
     const onRefresh = (status, message) => {
         setRefreshing(false);
@@ -35,30 +31,27 @@ export default function Home() {
             language,
             callback: onRefresh
         }));
-
     };
+
+    const renderItem = ({ item, i }) => (
+        <NewCard
+            {...item}
+            key={i}
+            layout={layout}
+        />
+    );
+
     return (
-        <View style={styles.container}>
+        <View>
             <StatusBar barStyle={colorScheme === 'light' ? 'dark-content' : 'light-content'} />
-            <ScrollView
-                style={styles[layout]}
-                refreshControl={(
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={handleRefresh}
-                        tintColor="red"
-                        colors={['red']}
-                    />
-                  )}
-            >
-                {getNews(language).map((e, index) => (
-                    <NewCard
-                        {...e}
-                        key={index}
-                        layout={layout}
-                    />
-))}
-            </ScrollView>
+            <MasonryList
+                key={layout}
+                onRefresh={handleRefresh}
+                refreshing={refreshing}
+                renderItem={renderItem}
+                data={getNews(language)}
+                numColumns={layout === 'grid' ? 2 : 1}
+            />
         </View>
     );
 }
